f510d5817ca186447652758a4e9aaf697ffe0afa
ztoc <bhu0444@gmail.com>
2024-10-08 00:03:07 -0400

    init

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HelloWorld = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello, World!</Text>
+    </View>
+  );
+};
+
+export default HelloWorld;
